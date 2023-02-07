assistant0411/tamagui

commit 790a2db5cdf9bb3995f4d3738ad6312c5e2a579c

feat(web): return merged tokens so both getTokens().size['-zsh'] and getTokens().size['0'] work, but edge case with numbered tokens making it impossible in typescript without some refactoring - you may be missing the ['0'] typed tokens but it will accept any string type and work at runtime until full fix lands

packages/web/src/config.ts
@@ -1,4 +1,4 @@
-import { ConfigListener, TamaguiInternalConfig, Tokens } from './types'
+import { ConfigListener, TamaguiInternalConfig, TokensMerged } from './types'
 
 let conf: TamaguiInternalConfig | null
 
@@ -26,8 +26,38 @@ export const getConfig = () => {
   return conf
 }
 
-export const getTokens = ({ prefixed = true }: { prefixed?: boolean } = {}): Tokens =>
-  prefixed ? conf!.tokensParsed : conf!.tokens
+let cached: TokensMerged
+export const getTokens = ({
+  prefixed,
+}: {
+  /**
+   * @deprecated prefixed true and false are merged together, no longer need this property
+   */
+  prefixed?: number
+} = {}): TokensMerged => {
+  return (cached ??= {
+    size: {
+      ...conf!.tokens,
+      ...conf!.tokensParsed,
+    },
+    space: {
+      ...conf!.tokens,
+      ...conf!.tokensParsed,
+    },
+    radius: {
+      ...conf!.tokens,
+      ...conf!.tokensParsed,
+    },
+    zIndex: {
+      ...conf!.tokens,
+      ...conf!.tokensParsed,
+    },
+    color: {
+      ...conf!.tokens,
+      ...conf!.tokensParsed,
+    },
+  } as any)
+}
 
 /**
  * Note: this is the same as `getTokens`

packages/web/types/config.d.ts
@@ -1,4 +1,4 @@
-import { ConfigListener, TamaguiInternalConfig, Tokens } from './types';
+import { ConfigListener, TamaguiInternalConfig, TokensMerged } from './types';
 export declare const setConfig: (next: TamaguiInternalConfig) => void;
 export declare const setConfigFont: (name: string, font: any, fontParsed: any) => void;
 export declare const getConfig: () => TamaguiInternalConfig<import("./types").CreateTokens<import("./types").VariableVal>, {
@@ -14,13 +14,16 @@ export declare const getConfig: () => TamaguiInternalConfig<import("./types").Cr
         [key: string]: any;
     };
 }, import("./types").GenericFonts>;
-export declare const getTokens: ({ prefixed }?: {
-    prefixed?: boolean | undefined;
-}) => Tokens;
+export declare const getTokens: ({ prefixed, }?: {
+    /**
+     * @deprecated prefixed true and false are merged together, no longer need this property
+     */
+    prefixed?: number | undefined;
+}) => TokensMerged;
 /**
  * Note: this is the same as `getTokens`
  */
-export declare const useTokens: () => import("./types").CreateTokens<import("./types").VariableVal>;
+export declare const useTokens: () => TokensMerged;
 export declare const getThemes: () => {
     [x: string]: {
         [x: string]: import("./createVariable").Variable<any>;

apps/sandbox/Sandbox.tsx
@@ -1,7 +1,7 @@
 import '@tamagui/core/reset.css'
 import '@tamagui/polyfill-dev'
 
-import { Stack, getTokens, styled, useStyle } from '@tamagui/core'
+import { Stack, Tokens, TokensParsed, getTokens, styled, useStyle } from '@tamagui/core'
 import { Moon } from '@tamagui/lucide-icons'
 import { useState } from 'react'
 import {
@@ -18,10 +18,9 @@ import {
 import { CodeExamplesInput } from './CodeExamplesInput'
 import config from './tamagui.config'
 
-const x = <Stack onLayout={(e) => e.nativeEvent.layout} />
+type x2 = TokensParsed['size']
 
-const tokens = getTokens()
-console.log(tokens.size['$0'], tokens.size['1'])
+const x = <Stack onLayout={(e) => e.nativeEvent.layout} />
 
 // import './wdyr'
 

apps/site/components/Hero.tsx
@@ -1,13 +1,12 @@
 import { useTint } from '@tamagui/logo'
 import { NextLink } from 'components/NextLink'
-import { memo, useEffect, useRef } from 'react'
+import { memo } from 'react'
 import {
   Button,
   ButtonText,
   H1,
   Paragraph,
   Spacer,
-  Square,
   Text,
   Theme,
   VisuallyHidden,

packages/theme-base/src/tokens.tsx
@@ -169,20 +169,20 @@ function postfixObjKeys<
 }
 
 export const radius = {
-  0: 0,
-  1: 3,
-  2: 5,
-  3: 7,
-  4: 9,
-  true: 9,
-  5: 10,
-  6: 16,
-  7: 19,
-  8: 22,
-  9: 26,
-  10: 34,
-  11: 42,
-  12: 50,
+  $0: 0,
+  $1: 3,
+  $2: 5,
+  $3: 7,
+  $4: 9,
+  $true: 9,
+  $5: 10,
+  $6: 16,
+  $7: 19,
+  $8: 22,
+  $9: 26,
+  $10: 34,
+  $11: 42,
+  $12: 50,
 }
 
 export const tokens = createTokens({

packages/theme-base/types/tokens.d.ts
@@ -668,20 +668,20 @@ export declare const color: {
     blue12Light: string | Variable<string>;
 };
 export declare const radius: {
-    0: number;
-    1: number;
-    2: number;
-    3: number;
-    4: number;
-    true: number;
-    5: number;
-    6: number;
-    7: number;
-    8: number;
-    9: number;
-    10: number;
-    11: number;
-    12: number;
+    $0: number;
+    $1: number;
+    $2: number;
+    $3: number;
+    $4: number;
+    $true: number;
+    $5: number;
+    $6: number;
+    $7: number;
+    $8: number;
+    $9: number;
+    $10: number;
+    $11: number;
+    $12: number;
 };
 export declare const tokens: {
     color: {
@@ -972,18 +972,18 @@ export declare const tokens: {
     radius: {
         0: Variable<number>;
         1: Variable<number>;
-        4: Variable<number>;
         2: Variable<number>;
         3: Variable<number>;
-        8: Variable<number>;
+        4: Variable<number>;
         true: Variable<number>;
-        12: Variable<number>;
         5: Variable<number>;
+        6: Variable<number>;
         7: Variable<number>;
+        8: Variable<number>;
         9: Variable<number>;
         10: Variable<number>;
-        6: Variable<number>;
         11: Variable<number>;
+        12: Variable<number>;
     };
     zIndex: {
         0: Variable<number>;

packages/web/src/types.tsx
@@ -93,7 +93,7 @@ export type ConfigListener = (conf: TamaguiInternalConfig) => void
 export type VariableVal = number | string | Variable
 export type VariableColorVal = string | Variable
 
-type GenericKey = string | number | symbol
+type GenericKey = string
 
 export interface CreateTokens<Val extends VariableVal = VariableVal> {
   color: { [key: GenericKey]: Val }
@@ -278,7 +278,35 @@ export type ThemeKeys = keyof ThemeDefinition
 export type ThemeParsed = {
   [key in ThemeKeys]: Variable<any>
 }
+
 export type Tokens = TamaguiConfig['tokens']
+
+export type TokensParsed = {
+  size: TokenPrefixed<Tokens['size']>
+  color: TokenPrefixed<Tokens['color']>
+  radius: TokenPrefixed<Tokens['radius']>
+  zIndex: TokenPrefixed<Tokens['zIndex']>
+  space: TokenPrefixed<Tokens['space']>
+}
+
+type TokenPrefixed<A extends { [key: string]: any }> = {
+  [key in Ensure$Prefix<keyof A>]: A[keyof A]
+}
+
+type Ensure$Prefix<A extends string | number | symbol> = A extends string
+  ? A extends `$${string}`
+    ? A
+    : `$${A}`
+  : never
+
+export type TokensMerged = {
+  size: TokensParsed['size'] & Tokens['size']
+  color: TokensParsed['color'] & Tokens['color']
+  radius: TokensParsed['radius'] & Tokens['radius']
+  zIndex: TokensParsed['zIndex'] & Tokens['zIndex']
+  space: TokensParsed['space'] & Tokens['space']
+}
+
 export type Shorthands = TamaguiConfig['shorthands']
 export type Media = TamaguiConfig['media']
 export type Themes = TamaguiConfig['themes']

packages/web/types/types.d.ts
@@ -54,7 +54,7 @@ export type ReactComponentWithRef<Props, Ref> = ForwardRefExoticComponent<Props 
 export type ConfigListener = (conf: TamaguiInternalConfig) => void;
 export type VariableVal = number | string | Variable;
 export type VariableColorVal = string | Variable;
-type GenericKey = string | number | symbol;
+type GenericKey = string;
 export interface CreateTokens<Val extends VariableVal = VariableVal> {
     color: {
         [key: GenericKey]: Val;
@@ -161,6 +161,26 @@ export type ThemeParsed = {
     [key in ThemeKeys]: Variable<any>;
 };
 export type Tokens = TamaguiConfig['tokens'];
+export type TokensParsed = {
+    size: TokenPrefixed<Tokens['size']>;
+    color: TokenPrefixed<Tokens['color']>;
+    radius: TokenPrefixed<Tokens['radius']>;
+    zIndex: TokenPrefixed<Tokens['zIndex']>;
+    space: TokenPrefixed<Tokens['space']>;
+};
+type TokenPrefixed<A extends {
+    [key: string]: any;
+}> = {
+    [key in Ensure$Prefix<keyof A>]: A[keyof A];
+};
+type Ensure$Prefix<A extends string | number | symbol> = A extends string ? A extends `$${string}` ? A : `$${A}` : never;
+export type TokensMerged = {
+    size: TokensParsed['size'] & Tokens['size'];
+    color: TokensParsed['color'] & Tokens['color'];
+    radius: TokensParsed['radius'] & Tokens['radius'];
+    zIndex: TokensParsed['zIndex'] & Tokens['zIndex'];
+    space: TokensParsed['space'] & Tokens['space'];
+};
 export type Shorthands = TamaguiConfig['shorthands'];
 export type Media = TamaguiConfig['media'];
 export type Themes = TamaguiConfig['themes'];

commit 1848cbf4f241f17c8aca6af10515696f7dc1b009

fix(core): bring back prefixed option on getConfig for times when iterating the keys is important, such as in the regression in 790a2db5 causing tooltip padding to collapse

packages/get-size/src/index.ts
@@ -11,7 +11,7 @@ export const stepTokenUpOrDown = (
   shift = 0,
   bounds = [0]
 ): Variable<number> => {
-  const tokens = getTokens()[type]
+  const tokens = getTokens({ prefixed: true })[type]
   const keysOrdered = tokensKeysOrdered.get(tokens) || Object.keys(tokens)
   const min = bounds[0] ?? 0
   const max = bounds[1] ?? keysOrdered.length - 1
@@ -21,8 +21,6 @@ export const stepTokenUpOrDown = (
   }
   const index = Math.min(max, Math.max(min, currentIndex + shift))
   const key = keysOrdered[index]
-  // console.log('keysOrdered', name, keysOrdered, currentIndex, shift, index, key)
-  // TODO
   // @ts-ignore
   return tokens[key] || tokens['$true']
 }

packages/tooltip/src/Tooltip.tsx
@@ -11,7 +11,7 @@ import {
   useInteractions,
   useRole,
 } from '@floating-ui/react-dom-interactions'
-import { useEvent, useId, withStaticProperties } from '@tamagui/core'
+import { SizeTokens, useEvent, useId, withStaticProperties } from '@tamagui/core'
 import { ScopedProps } from '@tamagui/create-context'
 import { FloatingOverrideContext, UseFloatingFn } from '@tamagui/floating'
 import { stepTokenUpOrDown } from '@tamagui/get-size'
@@ -35,12 +35,13 @@ const TooltipContent = React.forwardRef(
   ) => {
     const popperScope = usePopoverScope(__scopePopover)
     const popper = usePopperContext('PopperContent', popperScope['__scopePopper'])
+    const padding = props.size || popper.size || stepTokenUpOrDown('size', '$true', -2)
     return (
       <PopoverContent
         componentName="TooltipContent"
         disableRemoveScroll
         trapFocus={false}
-        padding={props.size || popper.size || '$2'}
+        padding={padding}
         pointerEvents="none"
         ref={ref}
         {...props}
@@ -136,12 +137,13 @@ export const Tooltip = withStaticProperties(
     const onCustomAnchorAdd = React.useCallback(() => setHasCustomAnchor(true), [])
     const onCustomAnchorRemove = React.useCallback(() => setHasCustomAnchor(false), [])
     const contentId = useId()
-    const size = `$${(stepTokenUpOrDown('size', '$true', -2) as any).key}` as any
+    const twoSmallerKey = stepTokenUpOrDown('size', '$true', -2).key
+    const size = `$${twoSmallerKey}`
 
     return (
       <FloatingOverrideContext.Provider value={useFloatingContext}>
         {/* default tooltip to a smaller size */}
-        <Popper size={size} {...popperScope} {...restProps}>
+        <Popper size={size as SizeTokens} {...popperScope} {...restProps}>
           <__PopoverProviderInternal
             scope={__scopePopover}
             popperScope={popperScope.__scopePopper}

packages/tooltip/types/Tooltip.d.ts
@@ -1,4 +1,5 @@
 import '@tamagui/polyfill-dev';
+import { SizeTokens } from '@tamagui/core';
 import { PopperProps } from '@tamagui/popper';
 import * as React from 'react';
 export type TooltipProps = PopperProps & {
@@ -22,39 +23,39 @@ export declare const TooltipGroup: ({ children, delay }: {
 export declare const Tooltip: React.FC<TooltipProps> & {
     Anchor: React.ForwardRefExoticComponent<Omit<import("react-native").ViewProps, "display" | "children" | "onLayout" | keyof import("react-native").GestureResponderHandlers> & import("@tamagui/core").ExtendBaseStackProps & import("@tamagui/core").TamaguiComponentPropsBase & import("@tamagui/core").WithThemeValues<import("@tamagui/core").StackStylePropsBase> & import("@tamagui/core").WithShorthands<import("@tamagui/core").WithThemeValues<import("@tamagui/core").StackStylePropsBase>> & import("@tamagui/core/types/reactNativeTypes").RNViewProps & Omit<{}, "elevation" | "fullscreen"> & {
         readonly fullscreen?: boolean | undefined;
-        readonly elevation?: import("@tamagui/core").SizeTokens | undefined;
+        readonly elevation?: SizeTokens | undefined;
     } & import("@tamagui/core").MediaProps<Partial<Omit<import("react-native").ViewProps, "display" | "children" | "onLayout" | keyof import("react-native").GestureResponderHandlers> & import("@tamagui/core").ExtendBaseStackProps & import("@tamagui/core").TamaguiComponentPropsBase & import("@tamagui/core").WithThemeValues<import("@tamagui/core").StackStylePropsBase> & import("@tamagui/core").WithShorthands<import("@tamagui/core").WithThemeValues<import("@tamagui/core").StackStylePropsBase>> & import("@tamagui/core/types/reactNativeTypes").RNViewProps & Omit<{}, "elevation" | "fullscreen"> & {
         readonly fullscreen?: boolean | undefined;
-        readonly elevation?: import("@tamagui/core").SizeTokens | undefined;
+        readonly elevation?: SizeTokens | undefined;
     }>> & import("@tamagui/core").PseudoProps<Partial<Omit<import("react-native").ViewProps, "display" | "children" | "onLayout" | keyof import("react-native").GestureResponderHandlers> & import("@tamagui/core").ExtendBaseStackProps & import("@tamagui/core").TamaguiComponentPropsBase & import("@tamagui/core").WithThemeValues<import("@tamagui/core").StackStylePropsBase> & import("@tamagui/core").WithShorthands<import("@tamagui/core").WithThemeValues<import("@tamagui/core").StackStylePropsBase>> & import("@tamagui/core/types/reactNativeTypes").RNViewProps & Omit<{}, "elevation" | "fullscreen"> & {
         readonly fullscreen?: boolean | undefined;
-        readonly elevation?: import("@tamagui/core").SizeTokens | undefined;
+        readonly elevation?: SizeTokens | undefined;
     }>> & React.RefAttributes<HTMLElement | import("react-native").View>>;
     Arrow: React.ForwardRefExoticComponent<Omit<import("react-native").ViewProps, "display" | "children" | "onLayout" | keyof import("react-native").GestureResponderHandlers> & import("@tamagui/core").ExtendBaseStackProps & import("@tamagui/core").TamaguiComponentPropsBase & import("@tamagui/core").WithThemeValues<import("@tamagui/core").StackStylePropsBase> & import("@tamagui/core").WithShorthands<import("@tamagui/core").WithThemeValues<import("@tamagui/core").StackStylePropsBase>> & import("@tamagui/core/types/reactNativeTypes").RNViewProps & Omit<{}, "elevation" | "fullscreen"> & {
         readonly fullscreen?: boolean | undefined;
-        readonly elevation?: import("@tamagui/core").SizeTokens | undefined;
+        readonly elevation?: SizeTokens | undefined;
     } & import("@tamagui/core").MediaProps<Partial<Omit<import("react-native").ViewProps, "display" | "children" | "onLayout" | keyof import("react-native").GestureResponderHandlers> & import("@tamagui/core").ExtendBaseStackProps & import("@tamagui/core").TamaguiComponentPropsBase & import("@tamagui/core").WithThemeValues<import("@tamagui/core").StackStylePropsBase> & import("@tamagui/core").WithShorthands<import("@tamagui/core").WithThemeValues<import("@tamagui/core").StackStylePropsBase>> & import("@tamagui/core/types/reactNativeTypes").RNViewProps & Omit<{}, "elevation" | "fullscreen"> & {
         readonly fullscreen?: boolean | undefined;
-        readonly elevation?: import("@tamagui/core").SizeTokens | undefined;
+        readonly elevation?: SizeTokens | undefined;
     }>> & import("@tamagui/core").PseudoProps<Partial<Omit<import("react-native").ViewProps, "display" | "children" | "onLayout" | keyof import("react-native").GestureResponderHandlers> & import("@tamagui/core").ExtendBaseStackProps & import("@tamagui/core").TamaguiComponentPropsBase & import("@tamagui/core").WithThemeValues<import("@tamagui/core").StackStylePropsBase> & import("@tamagui/core").WithShorthands<import("@tamagui/core").WithThemeValues<import("@tamagui/core").StackStylePropsBase>> & import("@tamagui/core/types/reactNativeTypes").RNViewProps & Omit<{}, "elevation" | "fullscreen"> & {
         readonly fullscreen?: boolean | undefined;
-        readonly elevation?: import("@tamagui/core").SizeTokens | undefined;
+        readonly elevation?: SizeTokens | undefined;
     }>> & {
         offset?: number | undefined;
-        size?: import("@tamagui/core").SizeTokens | undefined;
+        size?: SizeTokens | undefined;
     } & React.RefAttributes<unknown>>;
     Content: React.ForwardRefExoticComponent<import("@tamagui/popover").PopoverContentTypeProps & {
         __scopePopover?: import("@tamagui/create-context").Scope<any>;
     } & React.RefAttributes<unknown>>;
     Trigger: React.ForwardRefExoticComponent<Omit<import("react-native").ViewProps, "display" | "children" | "onLayout" | keyof import("react-native").GestureResponderHandlers> & import("@tamagui/core").ExtendBaseStackProps & import("@tamagui/core").TamaguiComponentPropsBase & import("@tamagui/core").WithThemeValues<import("@tamagui/core").StackStylePropsBase> & import("@tamagui/core").WithShorthands<import("@tamagui/core").WithThemeValues<import("@tamagui/core").StackStylePropsBase>> & import("@tamagui/core/types/reactNativeTypes").RNViewProps & Omit<{}, "elevation" | "fullscreen"> & {
         readonly fullscreen?: boolean | undefined;
-        readonly elevation?: import("@tamagui/core").SizeTokens | undefined;
+        readonly elevation?: SizeTokens | undefined;
     } & import("@tamagui/core").MediaProps<Partial<Omit<import("react-native").ViewProps, "display" | "children" | "onLayout" | keyof import("react-native").GestureResponderHandlers> & import("@tamagui/core").ExtendBaseStackProps & import("@tamagui/core").TamaguiComponentPropsBase & import("@tamagui/core").WithThemeValues<import("@tamagui/core").StackStylePropsBase> & import("@tamagui/core").WithShorthands<import("@tamagui/core").WithThemeValues<import("@tamagui/core").StackStylePropsBase>> & import("@tamagui/core/types/reactNativeTypes").RNViewProps & Omit<{}, "elevation" | "fullscreen"> & {
         readonly fullscreen?: boolean | undefined;
-        readonly elevation?: import("@tamagui/core").SizeTokens | undefined;
+        readonly elevation?: SizeTokens | undefined;
     }>> & import("@tamagui/core").PseudoProps<Partial<Omit<import("react-native").ViewProps, "display" | "children" | "onLayout" | keyof import("react-native").GestureResponderHandlers> & import("@tamagui/core").ExtendBaseStackProps & import("@tamagui/core").TamaguiComponentPropsBase & import("@tamagui/core").WithThemeValues<import("@tamagui/core").StackStylePropsBase> & import("@tamagui/core").WithShorthands<import("@tamagui/core").WithThemeValues<import("@tamagui/core").StackStylePropsBase>> & import("@tamagui/core/types/reactNativeTypes").RNViewProps & Omit<{}, "elevation" | "fullscreen"> & {
         readonly fullscreen?: boolean | undefined;
-        readonly elevation?: import("@tamagui/core").SizeTokens | undefined;
+        readonly elevation?: SizeTokens | undefined;
     }>> & React.RefAttributes<HTMLElement | import("react-native").View>>;
 };
 export {};

packages/web/src/config.ts
@@ -31,32 +31,35 @@ export const getTokens = ({
   prefixed,
 }: {
   /**
-   * @deprecated prefixed true and false are merged together, no longer need this property
+   * Force either with $ or without $ prefix
    */
   prefixed?: boolean
 } = {}): TokensMerged => {
-  return (cached ??= {
+  if (!conf) throw new Error(`never called createTamagui`)
+  if (prefixed === false) return conf.tokens
+  if (prefixed === true) return conf.tokensParsed
+  return (cached ??= Object.freeze({
     size: {
-      ...conf!.tokens['size'],
-      ...conf!.tokensParsed['size'],
+      ...conf.tokens['size'],
+      ...conf.tokensParsed['size'],
     },
     space: {
-      ...conf!.tokens['space'],
-      ...conf!.tokensParsed['space'],
+      ...conf.tokens['space'],
+      ...conf.tokensParsed['space'],
     },
     radius: {
-      ...conf!.tokens['radius'],
-      ...conf!.tokensParsed['radius'],
+      ...conf.tokens['radius'],
+      ...conf.tokensParsed['radius'],
     },
     zIndex: {
-      ...conf!.tokens['zIndex'],
-      ...conf!.tokensParsed['zIndex'],
+      ...conf.tokens['zIndex'],
+      ...conf.tokensParsed['zIndex'],
     },
     color: {
-      ...conf!.tokens['color'],
-      ...conf!.tokensParsed['color'],
+      ...conf.tokens['color'],
+      ...conf.tokensParsed['color'],
     },
-  } as any)
+  }) as any)
 }
 
 /**

packages/web/types/config.d.ts
@@ -16,7 +16,7 @@ export declare const getConfig: () => TamaguiInternalConfig<import("./types").Cr
 }, import("./types").GenericFonts>;
 export declare const getTokens: ({ prefixed, }?: {
     /**
-     * @deprecated prefixed true and false are merged together, no longer need this property
+     * Force either with $ or without $ prefix
      */
     prefixed?: boolean | undefined;
 }) => TokensMerged;